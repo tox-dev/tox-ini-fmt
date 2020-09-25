9d54393d0ec1822470e6b13068f567191be16f78
Bernát Gábor <bgabor8@bloomberg.net>
2020-09-25 14:18:05 +0100

    Add pre-commit hook, swap --in-place with --stdout (#9)

--- src/tox_ini_fmt/__main__.py
+++ src/tox_ini_fmt/__main__.py
@@ -8,11 +8,11 @@ from tox_ini_fmt.formatter import format_tox_ini
 def run(args: Optional[Sequence[str]] = None) -> int:
     opts = cli_args(sys.argv[1:] if args is None else args)
     formatted = format_tox_ini(opts.tox_ini)
-    if opts.in_place:
+    if opts.stdout:
+        print(formatted, end="")
+    else:
         opts.tox_ini.write_text(formatted)
         print(f"updated {opts.tox_ini}")
-    else:
-        print(formatted, end="")
     return 0
 
 
